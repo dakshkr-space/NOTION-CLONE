42d5570c197a50fb28b17d641245d55c0cab5fdf
Daksh <dakshkumarabc@gmail.com>
2026-06-06 19:22:40 +0530

    Added initial file organization and serving of static dashboard pages

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+func main() {
+
+	// User
+	type User struct {
+	ID            string    `json:"id"`
+	Email         string    `json:"email"`
+	Name          string    `json:"name"`
+	OAuthProvider string    `json:"oauth_provider"`
+	CreatedAt     time.Time `json:"created_at"`
+}
+
+   //Pages
+  type Page struct {
+	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
+	ParentID  *string   `json:"parent_id"` // nil = root page
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+}
